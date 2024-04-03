f54eb22a6394066d779dbd327b0d2622ebcc7de9
eliza0000 <azizbekovaeliza2005@gmail.com>
2024-04-03 17:24:52 +0600

    new files

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ def reverse_string(n):
 
 reverse_string("Python")
 
+
+num = int(input("САН ЖАЗ: "))
+
+if num % 2 == 0:
+    print('Жуп сан')
+else:
+    print('Так сан')
---
+++ test.py
@@ -0,0 +1,10 @@
+if znak == '+':
+    print(numbers1 + numbers2)
+elif znak == '-':
+    print(numbers1 - numbers2)
+elif znak == '*':
+    print(numbers1*numbers2)
+elif znak == '/':
+    print(numbers1/numbers2)
+else:
+    print('андай белги жок')
